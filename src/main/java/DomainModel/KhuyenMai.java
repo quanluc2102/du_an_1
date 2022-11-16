c2eb62a8ad833ceb699e0e486142ec46b1e539a3
DESKTOP-SS916DG\haha <vietttph20936@fpt.edu.vn>
2022-11-17 00:22:46 +0700

    cap nhat model

--- src/main/java/DomainModel/KhuyenMai.java
+++ src/main/java/DomainModel/KhuyenMai.java
@@ -50,7 +50,7 @@ public class KhuyenMai implements Serializable {
     public KhuyenMai() {
     }
 
-    public KhuyenMai(String id, String ma, Date ngayBatDau, Date ngayKetThuc, double giaGiam, int donVi, String moTa) {
+    public KhuyenMai(String id, String ma, Date ngayBatDau, Date ngayKetThuc, double giaGiam, boolean donVi, String moTa, List<HoaDon> listhHoaDons) {
         this.id = id;
         this.ma = ma;
         this.ngayBatDau = ngayBatDau;
@@ -58,6 +58,7 @@ public class KhuyenMai implements Serializable {
         this.giaGiam = giaGiam;
         this.donVi = donVi;
         this.moTa = moTa;
+        this.listhHoaDons = listhHoaDons;
     }
 
     public String getId() {
@@ -100,11 +101,11 @@ public class KhuyenMai implements Serializable {
         this.giaGiam = giaGiam;
     }
 
-    public int getDonVi() {
+    public boolean isDonVi() {
         return donVi;
     }
 
-    public void setDonVi(int donVi) {
+    public void setDonVi(boolean donVi) {
         this.donVi = donVi;
     }
 
@@ -116,9 +117,18 @@ public class KhuyenMai implements Serializable {
         this.moTa = moTa;
     }
 
+    public List<HoaDon> getListhHoaDons() {
+        return listhHoaDons;
+    }
+
+    public void setListhHoaDons(List<HoaDon> listhHoaDons) {
+        this.listhHoaDons = listhHoaDons;
+    }
+
     @Override
     public String toString() {
         return "KhuyenMai{" + "id=" + id + ", ma=" + ma + ", ngayBatDau=" + ngayBatDau + ", ngayKetThuc=" + ngayKetThuc + ", giaGiam=" + giaGiam + ", donVi=" + donVi + ", moTa=" + moTa + '}';
     }
 
+   
 }
